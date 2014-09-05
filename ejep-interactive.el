3d54f832cb30911f198d64a1896523f45a30e85b
Christian Köstlin <christian.koestlin@esrlabs.com>
2014-09-05 17:54:47 +0200

    added find-file and kill-buffer hooks

--- ejep-interactive.el
+++ ejep-interactive.el
@@ -1,20 +1,34 @@
+;;; ejep-interactive.el --- frontend functions
+;;; Commentary:
+;;; Code:
+
 (require 'ejep-communication)
 (require 'ejep-service)
 
 (defun ejep/content-sync/install-hook ()
-  "Installs the necessary hooks to sync the content with the jep backend."
+  "Install the necessary hooks to sync the content with the jep backend."
   (add-hook 'after-change-functions 'ejep/communication/send-buffer-update nil t))
 
 (defun ejep/disconnect ()
-    "disconnects from a backend process."
+  "Disconnect from a backend process."
   (interactive)
   (ejep/service/rm-process (buffer-file-name)))
 
 (defun ejep/connect ()
-    "connects the current buffer to an backend."
+  "Connect the current buffer to an backend."
   (interactive)
   (ejep/service/get-process (buffer-file-name))
   (ejep/communication/send-current-buffer)
-  (ejep/content-sync/install-hook))
+  (ejep/content-sync/install-hook)
+  (add-hook 'kill-buffer-hook 'ejep/disconnect nil t)
+  )
+
+(defun ejep/connect-if-possible ()
+  "Connect to a jep process if a matching .jep file is found."
+  (let* ((found (ejep/service/get-jepconfig-with-pattern-and-command (buffer-file-name))))
+    (if found (ejep/connect))))
+
+(add-hook 'find-file-hook 'ejep/connect-if-possible)
 
 (provide 'ejep-interactive)
+;;; ejep-interactive ends here
